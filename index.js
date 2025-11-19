fa968255bd9dbf3210b98db3acec22ff41895ec1
Felipebali <felipebaliski@gmail.com>
2025-11-18 21:35:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -263,7 +263,7 @@ if (readBotPath.includes(creds)) {
 kanekiAIJadiBot({pathkanekiAIJadiBot: botPath, m: null, conn, args: '', usedPrefix: '/', command: 'serbot'})
 }}}}
 
-const pluginFolder = global.__dirname(join(__dirname, './plugins/index'))
+const pluginFolder = global.__dirname(join(__dirname, './plugins'))
 const pluginFilter = (filename) => /\.js$/.test(filename)
 global.plugins = {}
 async function filesInit() {
